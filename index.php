fbcfcbb2c65a6eb9e2a704903b5a703029c3a6cd
kernelk14 <khyleisaias@gmail.com>
2026-07-27 00:14:06 +0800

    added posts/rants

--- index.php
+++ index.php
@@ -20,6 +20,39 @@ if (isset($_POST['btn_submit'])) {
     else echo "<script>console.log('There are problems: " . $conn->error . "')</script>";
 }
 
+function printCard($row) {
+    $uname = htmlspecialchars(trim($row['uname']));
+    $cont = htmlspecialchars(trim($row['content']));
+
+    if (strlen(trim($cont)) > 50) {
+        echo <<<EOT
+            <script>
+                console.log('$uname exceeded 50 words');
+            </script>
+            <style>
+                .cardscroll {
+                    overflow-y: scroll;
+                    scrollbar-color: #383838 #181818;;
+                    scrollbar-width: thin;
+                }
+                .cardscroll::-webkit-scrollbar-track {background-color: #181818;}
+            </style>
+            <div class="card cardscroll">
+                <h4>$uname</h4>
+                <p>$cont</p>
+            </div>
+        EOT;
+    }
+    else {
+        echo <<<EOT
+        <div class="card">
+            <h4>$uname</h4>
+            <p>$cont</p>
+        </div>
+        EOT;
+    }
+}
+
 
 ?>
 
@@ -27,6 +60,7 @@ if (isset($_POST['btn_submit'])) {
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Ranters</title>
+        <link rel="stylesheet" href="style.css" />
         <script>
             if (window.history.replaceState) {
                 window.history.replaceState(null, null, window.location.href);
@@ -36,12 +70,22 @@ if (isset($_POST['btn_submit'])) {
     <body>
         <center>
             <form method="POST">
-                <input type="text" name="uname" id="uname" placeholder="Enter your stage name" autocomplete="off"/>
+                <input class="textbox" type="text" name="uname" id="uname" placeholder="Enter your stage name" autocomplete="off"/>
                 <br />
-                <textarea name="cont" id="cont" autocomplete="off" placeholder="Enter your rant+"></textarea>
+                <textarea class="textarea" name="cont" id="cont" autocomplete="off" placeholder="Enter your rant"></textarea>
                 <br />
                 <input type="submit" name="btn_submit" value="Submit" />
             </form>
         </center>
+        <hr />
+        <div class="content-container">
+            <?php
+                $query = "SELECT * FROM rants ORDER BY time_created DESC";
+                $result = $conn->query($query);
+                while ($row = $result->fetch_assoc()) {
+                    printCard($row);
+                }
+            ?>
+        </div>
     </body>
 </html>
